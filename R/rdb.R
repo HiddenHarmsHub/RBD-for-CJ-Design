0192f9b4900ac583b4e3154a0a70139d58717027
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-06-10 11:55:00 +0100

    trying to be more Ry

--- R/rdb.R
+++ R/rdb.R
@@ -1,43 +1,40 @@
-verbose <- FALSE
+verbose <- TRUE
 seed <- TRUE
 start <- 10
 
-rbd <- function(data, tol = 1e-6, col = min(10, ncol(data))) {
+rbd <- function(data, tol = 1e-6, max_cols = min(10, ncol(data))) {
 
   if (missing(data)) {
     stop("RBD needs at least one input, a matrix")
   }
-
   # preparation of the algorithm
-  nr <- nrow(data)
-  nc <- ncol(data)
-  bases <- matrix(0, nr, col)
-  trans_mat <- matrix(0, col, nc)
-  xi_flag <- integer(col)
-
+  col_count <- ncol(data)
+  bases <- matrix(0, nrow(data), max_cols)
+  trans_mat <- matrix(0, max_cols, col_count)
+  # select our first column (either using the seed or at random)
   if (seed) {
-    xi_flag[1] <- start
+    xi_flag <- start
   } else {
-    xi_flag[1] <- sample(nc, 1)
+    xi_flag <- sample(col_count, 1)
   }
-  i <- 1
-  cur_err <- tol + 1
+  cur_err <- tol + 1  # just to get the loop going
 
   # Preparation for efficient error evaluation
-  ftf <- colSums(t(data)^2)  # could we just use rowSums without the t()?
-  ataatxi <- matrix(0, nc, col)
+  ftf <- rowSums(data^2)
+  ataatxi <- matrix(0, col_count, max_cols)
 
   # The RBD greedy algorithm
-  while (i <= col && cur_err > tol) {
+  i <- 1
+  while (i <= max_cols && cur_err > tol) {
     if (verbose) {
       cat("we are on loop interation ", i, "\n")
-      cat("starting column position is ", xi_flag[i], "\n")
+      cat("using column in position ", xi_flag, "\n")
     }
-    bi_cand <- data[, xi_flag[i]]
+    bi_cand <- data[, xi_flag]
 
     # Inside: Gram-Schmidt orthonormalization of the current candidate with all previously chosen basis vectors
     for (j in if (i > 1) 1:(i-1) else integer(0)) {
-      bi_cand <- bi_cand - (t(bi_cand) %*% bases[, j]) * bases[, j]
+      bi_cand <- bi_cand - as.vector(t(bi_cand) %*% bases[, j, drop = FALSE]) * bases[, j]
     }
     normi <- sqrt(t(bi_cand) %*% bi_cand)
     if (verbose) {
@@ -49,10 +46,9 @@ rbd <- function(data, tol = 1e-6, col = min(10, ncol(data))) {
       trans_mat <- trans_mat[1 : i - 1, ]
       break
     } else {
-      bases[, i] <- bi_cand / normi
+      bases[, i] <- bi_cand / as.vector(normi)
     }
     trans_mat[i, ] <- t(bases[, i]) %*% data
-
     ataatxi[, i] <- t(data) %*% bases[, i]
 
     # Inside: Efficiently go through all the columns to identify where the error would be the largest if we were to use the current space for compression.
@@ -65,8 +61,8 @@ rbd <- function(data, tol = 1e-6, col = min(10, ncol(data))) {
     temp_pos <- which.max(errord)
 
     # Mark this location for the next round
-    if (i < col) {
-      xi_flag[i + 1] <- temp_pos
+    if (i < max_cols) {
+      xi_flag <- temp_pos
     }
     if (verbose) {
       cat("cur_err is ", cur_err, "\n")
@@ -85,4 +81,4 @@ rbd <- function(data, tol = 1e-6, col = min(10, ncol(data))) {
 
 test_df <- read.csv("data/C_Barnet.csv", header = TRUE)
 test_matrix <- as.matrix(test_df)
-result <- rbd(test_matrix, 1e-13, 22)
+result <- rbd(test_matrix, 1e-13)
